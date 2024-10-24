e65e26a0d3494d72a1333cd0d647da63a3ac0f5d
Arkhip McNutt <ortiz47@mail.ru>
2024-10-24 11:15:42 +0300

    client must be working now ig

--- client/internal/client/client.go
+++ client/internal/client/client.go
@@ -1,8 +1,10 @@
 package client
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"time"
@@ -15,6 +17,7 @@ type Client interface {
 	Del(key string) error
 }
 
+// client implementation over HTTP
 type HTTPClient struct {
 	http http.Client
 }
@@ -31,24 +34,23 @@ func (c *HTTPClient) Add(key, value string, ttl time.Duration) error {
 	// calculating expiration time
 	expiresAt := time.Now().Add(ttl)
 
-	req, err := http.NewRequest("POST", "http:/localhost:8080/api/v1/add", nil)
+	req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/add", nil)
 	if err != nil {
 		return err
 	}
 
-	form := url.Values{}
-	form.Add("key", key)
-	form.Add("value", value)
-	form.Add("expires_at", expiresAt.String())
-	req.PostForm = form
+	req.PostForm = c.createPostForm(key, value, expiresAt.String())
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	_, err = c.http.Do(req)
+	// send http request
+	res, err := c.http.Do(req)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	_, err = c.handleResponse(res)
+
+	return err
 }
 
 func (c *HTTPClient) Set(key, value string, ttl time.Duration) error {
@@ -59,73 +61,110 @@ func (c *HTTPClient) Set(key, value string, ttl time.Duration) error {
 	// calculating expiration time
 	expiresAt := time.Now().Add(ttl)
 
-	req, err := http.NewRequest("POST", "http:/localhost:8080/api/v1/add", nil)
+	req, err := http.NewRequest("POST", "http://localhost:8080/api/v1/set", nil)
 	if err != nil {
 		return err
 	}
 
-	form := url.Values{}
-	form.Add("key", key)
-	form.Add("value", value)
-	form.Add("expires_at", expiresAt.String())
-	req.PostForm = form
+	req.PostForm = c.createPostForm(key, value, expiresAt.String())
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	_, err = c.http.Do(req)
+	// send http request
+	res, err := c.http.Do(req)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	_, err = c.handleResponse(res)
+
+	return err
 }
 
 func (c *HTTPClient) Get(key string) (string, error) {
-	req, err := http.NewRequest("GET", "http:/localhost:8080/api/v1/add", nil)
+	req, err := http.NewRequest("GET", "http://localhost:8080/api/v1/get", nil)
 	if err != nil {
 		return "", err
 	}
 
-	req.URL.Query().Add("key", key)
+	// add query params
+	queryParams := req.URL.Query()
+	queryParams.Add("key", key)
+	req.URL.RawQuery = queryParams.Encode()
 
-	_, err = c.http.Do(req)
+	// send the request
+	res, err := c.http.Do(req)
 	if err != nil {
 		return "", err
 	}
 
-	return "", nil
+	return c.handleResponse(res)
 }
 
 func (c *HTTPClient) Del(key string) error {
-
-	req, err := http.NewRequest("DELETE", "http:/localhost:8080/api/v1/add", nil)
+	req, err := http.NewRequest("DELETE", "http://localhost:8080/api/v1/del", nil)
 	if err != nil {
 		return err
 	}
 
-	req.URL.Query().Add("key", key)
+	// add query params
+	queryParams := req.URL.Query()
+	queryParams.Add("key", key)
+	req.URL.RawQuery = queryParams.Encode()
 
-	_, err = c.http.Do(req)
+	// send the request
+	res, err := c.http.Do(req)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	_, err = c.handleResponse(res)
+
+	return err
 }
 
+func (c *HTTPClient) handleResponse(res *http.Response) (msg string, err error) {
+	// read response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return msg, err
+	}
+
+	// body message
+	msg = string(body)[:len(body)-1] // for some reason theres one redundant \n
+
+	// check for server-side handled errors
+	if res.Status != "200 OK" {
+		return msg, errors.New(msg)
+	}
+
+	return msg, err
+}
+
+func (c *HTTPClient) createPostForm(key, value, expiresAt string) url.Values {
+	form := url.Values{}
+	form.Add("key", key)
+	form.Add("value", value)
+	form.Add("expires_at", expiresAt)
+
+	return form
+}
+
+// aggregate over client and parser
 type ClientApp struct {
 	cl Client
-	p  parser
+	p  Parser
 }
 
 func New(client Client) *ClientApp {
 	return &ClientApp{
 		cl: client,
-		p:  parser{},
+		p:  Parser{},
 	}
 }
 
+// run the entire client app
 func (app ClientApp) Run() {
-	op, key, val, ttl, err := app.p.parse()
+	op, key, val, ttl, err := app.p.Parse()
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
@@ -158,9 +197,10 @@ func (app ClientApp) Run() {
 
 }
 
-type parser struct{}
+// incoming flag params parser
+type Parser struct{}
 
-func (p parser) parse() (op, key, val *string, ttl *time.Duration, err error) {
+func (p Parser) Parse() (op, key, val *string, ttl *time.Duration, err error) {
 	op = flag.String("op", "", "operation to be executed")
 	key = flag.String("key", "", "key to be inserted")
 	val = flag.String("val", "", "value to be paired with the key")
