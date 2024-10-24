e95ee525c9123b118071c11e4e1f3cdd582c9e4d
Arkhip McNutt <ortiz47@mail.ru>
2024-10-24 07:25:20 +0300

    updated server dir to storage, idk why

--- server/cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/cutlery47/key-value-storage/server"
-
-func main() {
-	server.Run()
-}
--- server/go.mod
+++
@@ -1,4 +0,0 @@
-module github.com/cutlery47/key-value-storage/server
-
-
-go 1.23.2
